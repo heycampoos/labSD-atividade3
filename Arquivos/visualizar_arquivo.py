d26c67ab52d327de10228d8ca034344043075ef7
Felipe Campos <58793482+heycampoos@users.noreply.github.com>
2024-10-31 21:12:26 -0300

    Ajustes de código para windows

--- Arquivos/visualizar_arquivo.py
+++ Arquivos/visualizar_arquivo.py
@@ -1,15 +1,17 @@
 import os
+pasta_padrao = r'C:/Users/felip/Downloads'
 
-def visualizar_arquivo(filepath):
+def visualizar_arquivo(filename):
+    # Concatena a pasta padrão com o nome do arquivo
+    filepath = os.path.join(pasta_padrao, filename)
+    
     if os.path.exists(filepath):
         if filepath.endswith('.txt'):
             with open(filepath, 'r') as f:
                 conteudo = f.read()
                 print(conteudo)
-        elif filepath.endswith(('.png', '.jpg', '.jpeg', '.gif')):
-            os.system(f'xdg-open {filepath}')  # No Linux
-        elif filepath.endswith('.mp4'):
-            os.system(f'xdg-open {filepath}')  # No Linux
+        elif filepath.endswith(('.png', '.jpg', '.jpeg', '.gif', '.mp4')):
+            os.startfile(filepath)  # No Windows
         else:
             print("Tipo de arquivo não suportado.")
     else:
